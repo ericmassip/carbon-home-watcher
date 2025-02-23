827031a51082ca8b1efdcdf14056bbd49ef34916
Eric Massip <ericmassip1@gmail.com>
2025-02-23 17:53:23 +0100

    Create appliance through the appliance form and update the appliances table with oob swap instead of reloading the whole page

--- carbonhomewatcher/views.py
+++ carbonhomewatcher/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, TemplateView
-from django_htmx.http import trigger_client_event
 from django_tables2 import tables
 
 from carbonhomewatcher.forms import ApplianceForm
@@ -15,29 +14,25 @@ class ApplianceTable(tables.Table):
         orderable = False
 
 
+def get_appliance_table():
+    return ApplianceTable(Appliance.objects.all())
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
 
-
-class ApplianceTableView(TemplateView):
-    template_name = "home.html#appliance-table"
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["appliance_table"] = ApplianceTable(Appliance.objects.all())
+        context["appliance_table"] = get_appliance_table()
         return context
 
 
 class ApplianceCreateView(CreateView):
     model = Appliance
     form_class = ApplianceForm
-    template_name = "home.html#appliance-form"
+    template_name = "partials/appliance_form.html"
 
     def form_valid(self, form):
         self.object = form.save()
-        response = render(
-            self.request,
-            "home.html#appliance-created-alert",
-            {"appliance": self.object},
-        )
-        return trigger_client_event(response, "newAppliance")
+        context = {"appliance_table": get_appliance_table(), "appliance": self.object}
+        return render(self.request, "partials/appliance_table.html", context)
