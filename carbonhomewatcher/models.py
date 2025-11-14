a01fa2b17ac210f0d2942e399fd4f8ac3a93f007
Eric Massip <ericmassip1@gmail.com>
2025-11-14 21:37:34 +0000

    Set is_active field to default to True

--- carbonhomewatcher/models.py
+++ carbonhomewatcher/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Appliance(models.Model):
     name = models.CharField(max_length=100)
     power = models.FloatField()
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ("name", )
